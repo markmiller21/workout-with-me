470e7f9abd0cc031f3b044323da5f9f483f3113b
Jenny Lee <leejenny0403@gmail.com>
2015-10-08 18:09:52 -0400

    change description text to should show all activities

--- spec/features/activity_spec.rb
+++ spec/features/activity_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Activity Page", :type => :feature do
+RSpec.feature "Activities Page", :type => :feature do
   let(:log_me_in) {
     user = User.create(name: "Jenny", email: "jenny@example.com", password_digest: "jenny", age: 25, gender: "Female", description: "I'm confused")
     visit root_path
@@ -11,7 +11,7 @@ RSpec.feature "Activity Page", :type => :feature do
     log_me_in
   end
 
-  scenario "User can view activities" do
+  scenario "should show all activities" do
     visit activities_path
     expect(page).to have_content("Select Your Activities")
   end
