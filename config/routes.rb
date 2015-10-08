c6dad42a2b04f2643aaab676b80e69f4cd1271fb
Jenny Lee <leejenny0403@gmail.com>
2015-10-08 17:59:11 -0400

    add activities route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'sessions#index'
 
   resources :matches
+  resources :activities
 
   get '/session' => 'sessions#index'
   get '/login' => 'sessions#new'
--- spec/features/activity_spec.rb
+++ spec/features/activity_spec.rb
@@ -1,19 +1,19 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.feature "Activity Page", :type => :feature do
-#   let(:log_me_in) {
-#     user = create(:user)
-#     visit login_path
-#     click_link "Login Here"
-#   }
+RSpec.feature "Activity Page", :type => :feature do
+  let(:log_me_in) {
+    user = User.create(name: "Jenny", email: "jenny@example.com", password_digest: "jenny", age: 25, gender: "Female", description: "I'm confused")
+    visit root_path
+    click_link "Login Here"
+  }
 
-#   before :each do
-#     log_me_in
-#   end
+  before :each do
+    log_me_in
+  end
 
-#   scenario "User can view activities" do
-#     visit activities_path
-#     expect(page).to have_content("Select Your Activities")
-#   end
+  scenario "User can view activities" do
+    visit activities_path
+    expect(page).to have_content("Select Your Activities")
+  end
 
-# end
+end
