e719e6663beda1ef857d3c0a1d750ee1a99cff7a
Navraj Nat <nnat425@gmail.com>
2015-10-08 18:04:50 -0400

    Test is asking right question to find user

--- spec/features/match_spec.rb
+++ spec/features/match_spec.rb
@@ -3,17 +3,19 @@ require 'rails_helper'
 describe 'possible match' do
 
   let(:log_me_in){
-  user = create(:user)
-  visit login_path
+    @logged_in_user = User.create(name:"raj",password_digest:"raj",age:22,gender:"Male",description:"yolo",email:"raj@raj.com")
+  # user = create(:user)
+  visit root_path
   click_link "Login Here"
 }
 
 
-  it 'page contains potential user on the page' do
-    visit new_match_path
-    potential_match = create(:user)
-    expect(page).to have_content potential_match.name
-  end
+it 'page contains potential user on the page' do
+  log_me_in
+  visit new_match_path
+  @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
+  expect(page).to have_content @potential_user.name
+end
 
 
 end
