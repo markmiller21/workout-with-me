b079812f8c8f8629b56b1f0a30faa15e7974e963
Jenny Lee <leejenny0403@gmail.com>
2015-10-08 17:25:45 -0400

    add test for user can view activities

---
+++ spec/features/activity_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "Activity Page", :type => :feature do
+  let(:log_me_in) {
+    user = create(:user)
+    visit login_path
+    click_link "Login Here"
+  }
+
+  before :each do
+    log_me_in
+  end
+
+  scenario "User can view activities" do
+    visit activities_path
+    expect(page).to have_content("Select Your Activities")
+  end
+
+end
