4ae8962645978b6c401cceba1c16d71fe20ed46e
Jenny Lee <leejenny0403@gmail.com>
2015-10-08 18:50:45 -0400

    create variables for new and create activity methods

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,11 @@
 class ActivitiesController < ApplicationController
 
   def new
+    @activity = Activity.new
   end
 
   def create
+    activity = Activity.create(params[:activity])
   end
 
   def show
