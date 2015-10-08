96fb0bc49636682cbb977190a2418b58435ec9ff
Navraj Nat <nnat425@gmail.com>
2015-10-08 18:40:56 -0400

    Add comment in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,6 @@ class SessionsController < ApplicationController
   	#this is temporary for testing purposes
   	session[:user_id] = 1
   	redirect_to match_path(User.find(1))
+    #Redirection needs to change to different user based off algorthim
   end
 end
