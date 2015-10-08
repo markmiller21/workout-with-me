0c07138e9711c056506550f4b0b04aab93f3cd51
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-08 14:58:46 -0400

    complete migrations

--- db/migrate/20151008183133_create_matches.rb
+++ db/migrate/20151008183133_create_matches.rb
@@ -1,6 +1,11 @@
 class CreateMatches < ActiveRecord::Migration
   def change
     create_table :matches do |t|
+    	t.references :initiator, null: false
+    	t.references :responder, null: false
+    	t.boolean :accepted, default: false
+
+    	t.timestamps null: false
     end
   end
 end
