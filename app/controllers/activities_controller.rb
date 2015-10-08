2809bd30676471c89a63fbde78201a789712aa1b
Jenny Lee <leejenny0403@gmail.com>
2015-10-08 19:10:43 -0400

    create seed files for sample activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,15 +1,6 @@
 class ActivitiesController < ApplicationController
 
-  def new
-    @activity = Activity.new
-  end
-
-  def create
-    activity = Activity.create(params[:activity])
-    redirect_to activities_path
-  end
-
-  def show
+  def index
     @activities = Activity.all
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
 jenny = User.create(name: "jenny", email: "jenny@jenny.com", password_digest: "jenny", age: 52, gender: "neutral", description: "I enjoy being gender neutral")
 raj = User.create(name: "raj", email: "raj@raj.com", password_digest: "jenny", age: 52, gender: "neutral", description: "I enjoy being gender neutral")
+
+lifting = Activity.create(name: "Lifting", image: "http://www.womenshealthmag.com/sites/womenshealthmag.com/files/images/beauty-weights-art.jpg")
+
+tennis = Activity.create(name: "Tennis", image: "http://sport.maths.org/content/sites/sport.maths.org/files/iStock_Tennis_crop.jpg")
+
+basketball = Activity.create(name: "Basketball", image: "http://www.rowan.edu/studentaffairs/Rec/sports/W.%20Basketball.jpg")
+
+spin = Activity.create(name: "Spin", image: "http://heuserhealth.com/wp-content/uploads/2014/01/iStock_000012790937Medium.jpg")
+
+soccer = Activity.create(name: "Soccer", image: "http://rezatehranicohen.info/wp-content/uploads/rezatehranicohen-info/sites/593/Document.jpeg")
