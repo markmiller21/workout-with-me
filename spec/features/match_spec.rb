76ae9ce9cf9efdb07bb2db6a931cd2676fcf1b04
Navraj Nat <nnat425@gmail.com>
2015-10-08 17:25:26 -0400

    comment out test

--- spec/features/match_spec.rb
+++ spec/features/match_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
+# let(:log_me)
+
 describe 'possible match' do
 
-it 'page contains another user on the page' do
-logged_in_user = create(:user)
 
 
-end
+# it 'page contains another user on the page' do
+# logged_in_user = create(:user)
+
+
+# end
 
 end
