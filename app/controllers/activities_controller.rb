7247b7f2786256c8487e1b5a445d0e7d57bf02b8
Jenny Lee <leejenny0403@gmail.com>
2015-10-08 18:58:24 -0400

    add activity name and image for each activity on activity index page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,9 +6,11 @@ class ActivitiesController < ApplicationController
 
   def create
     activity = Activity.create(params[:activity])
+    redirect_to activities_path
   end
 
   def show
+    @activities = Activity.all
   end
 
 end
