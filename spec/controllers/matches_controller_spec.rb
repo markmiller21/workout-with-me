4587ee9657e3137f938a48d887127ad5f3cb2ae2
Navraj Nat <nnat425@gmail.com>
2015-10-09 11:26:22 -0400

    can render show view

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -4,9 +4,11 @@ describe MatchesController do
 
   describe "GET #show" do
 
+
     it "renders the #show view" do
-      get :show, id: create(:user)
-      response.should render_template :show
+       @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
+      get :show, id: @potential_user.id
+      expect(response).to render_template :show
     end
   end
 
