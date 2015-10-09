13a74c53db09ac71fa23d5762d13a9a34fc16ad9
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 15:20:01 -0400

    add session methods for omniauth:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
   def create
 	  auth_hash = request.env['omniauth.auth']
     @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    binding.pry
     if @authorization
       binding.pry
       render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
