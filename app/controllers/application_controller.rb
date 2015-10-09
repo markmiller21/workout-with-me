b935d2c4070bf713d2b8111a273ff9794be7954b
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 18:21:21 -0400

    build MVP current user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
+
+  def current_user
+  	if session[:user_id]
+  		return User.find_by(id: session[:user_id])
+  	else
+  		return nil
+  	end
+  end
+
+  
 end
