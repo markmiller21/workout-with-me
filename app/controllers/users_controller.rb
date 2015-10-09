b2488dd7c8f9160f3ecfb1e7be600060d034ab1d
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 18:12:28 -0400

    change username in params to email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :username, :password, :gender, :description, :age)
+    params.require(:user).permit(:name, :email, :password, :gender, :description, :age)
   end
 end
