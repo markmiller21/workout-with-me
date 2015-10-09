2148da8802a737382d2fc94e98c132a22806dbfa
Navraj Nat <nnat425@gmail.com>
2015-10-08 20:21:08 -0400

    create method for match controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,22 @@
 class MatchesController < ApplicationController
 
 	def show
+    @match = Match.new
     @potential_match = User.find_by(id: params[:id])
-	end
-
-  def create
-
   end
 
-end
+  def create
+    match = Match.build(responder_id: params[:match][:responder_id],initiator_id: current_user)
+    if Match.where(responder_id: current_user, initiator_id:params[:match][:responder_id])
+      flash[:match] = "You have been matched!"
+      redirect match_show(User.all.sample.id)
+      match.update(accepted: 1)
+    end
+
+
+      match = Match.create(responder_id: params[:match][:responder_id],initiator_id: current_user)
+
+
+    end
+
+  end
