f1cfd9f6d18ce83c525575cee51212018d62a7db
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 15:00:00 -0400

    add connection to facebook but not able to save user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,17 @@ class SessionsController < ApplicationController
 
   def create
 	  auth_hash = request.env['omniauth.auth']
-	  render :text => auth_hash.inspect
+    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    if @authorization
+      binding.pry
+      render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+    else
+      binding.pry
+      user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
+      user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+      user.save
+   
+      render :text => "Hi #{user.name}! You've signed up."
+    end
 	end
 end
