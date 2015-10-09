dd4500edad65b7089b4d3625f0441359ff52aafd
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 16:22:25 -0400

    change user_email to email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1008625599180843', 'ff8dbae805565b26599d7c4f41a4e7d6',
-  scope: 'user_email'
+  scope: 'email'
 end
