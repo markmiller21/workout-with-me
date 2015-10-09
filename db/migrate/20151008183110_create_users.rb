7bac4c5f97deb1ca735c95f246642e68c3860991
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 17:32:04 -0400

    remove all authorizatoin and oauth associations

--- db/migrate/20151008183110_create_users.rb
+++ db/migrate/20151008183110_create_users.rb
@@ -2,10 +2,10 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
     	t.string :name, null: false
-    	t.string :email#, null: false, limit: 100
-    	t.string :password_digest #, null: false
-    	t.integer :age #, null: false, limit: 3
-    	t.string :gender #, null: false
+    	t.string :email, null: false, limit: 100
+    	t.string :password_digest, null: false
+    	t.integer :age, null: false, limit: 3
+    	t.string :gender, null: false
     	t.string :description, limit: 500
 
     	t.timestamps null: false
