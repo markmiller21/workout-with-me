2ea9445c41ebb94ebbe00793ef75bca4799a541d
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 18:42:33 -0400

    format ratingmodel file

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,5 @@ class Rating < ActiveRecord::Base
   belongs_to :ratee, :class_name => 'User'
   belongs_to :rater, :class_name => 'User'
 
-  	validates :rank, :presence => true
-
+  validates :rank, :presence => true
 end
