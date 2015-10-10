87e1f4cbc03f0796aab85ead4b848132ec1ecc5b
Navraj Nat <nnat425@gmail.com>
2015-10-10 17:26:07 -0400

    Add test to match_controller_spec

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -6,14 +6,21 @@ describe MatchesController do
 
 
     it "renders the #show view" do
-       @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
-      get :show, id: @potential_user.id
-      expect(response).to render_template :show
+     @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
+     get :show, id: @potential_user.id
+     expect(response).to render_template :show
+   end
+
+
+   describe "GET #index" do
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
     end
-
-
-
   end
+
+
+end
 
 
 end
