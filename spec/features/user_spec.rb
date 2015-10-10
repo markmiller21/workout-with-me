7afb81a1b68d2809e4945da887f009d953bb546d
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-09 21:47:06 -0400

    add log_me_in method

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,1 +1,14 @@
-user_spec.rb
+require 'rails_helper'
+
+describe 'Logging in Process' do 
+	let(:log_me_in){
+		user = create(:user)
+		visit_login_path
+		within("#login") do
+	    fill_in 'Username', :with => user.username
+	    fill_in 'Password', :with => user.password
+	    click_button 'Login'
+  	end
+	}
+
+end
