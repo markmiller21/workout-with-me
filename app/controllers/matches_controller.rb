aa0867e8f8d1d777a2b2e014cfc41b82b44818d9
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-10 13:17:55 -0400

    add rough logic

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -9,14 +9,34 @@ class MatchesController < ApplicationController
   end
 
   def create
-    existing_match = Match.find_by(responder_id: current_user.id, initiator_id:params[:match][:responder_id])
-    if existing_match
-      flash[:match] = "You have been matched!"
-      existing_match.update_attributes(accepted: 1)
-    else
-      Match.create(responder_id: params[:match][:responder_id],initiator_id: current_user.id)
+    # existing_match = Match.find_by(responder_id: current_user.id, initiator_id:params[:match][:responder_id])
+    # if existing_match
+    #   flash[:match] = "You have been matched!"
+    #   existing_match.update_attributes(accepted: 1)
+    # else
+    #   Match.create(responder_id: params[:match][:responder_id],initiator_id: current_user.id)
+    # end
+    # redirect_to match_path(current_user.initiator_matches.where(accepted: 2).sample.responder_id)
+
+    # Gather all the users with a similar interest
+    potential_matches = []
+    current_user.acivities.each do |activity|
+      activity.users.each do |user|
+        potential_matches << user
+      end
     end
-    redirect_to match_path(current_user.initiator_matches.where(accepted: 2).sample.responder_id)
+    for x in 0..potential_matches.length
+      if Match.where(initiator_id: current_user.id, responder_id: potential_matches[x])
+        next
+      elsif Match.where(initiator_id: potential_matches[x], responder_id: current_user.id, accepted: 1)
+        next
+      elsif Match.where(initiator_id: potential_matches[x], responder_id: current_user.id, accepted: -1)
+        next
+      else
+        print "Atleast we made it this far.  I am going to return #{potential_matches[x].name} who has #{potential_matches[x].activities}"
+        return potential_matches[x]
+      end          
+    end
   end
 
 end
