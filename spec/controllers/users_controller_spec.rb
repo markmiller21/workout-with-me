124db3cacdb2b751b076885cedef1b11189a5273
Navraj Nat <nnat425@gmail.com>
2015-10-10 16:45:42 -0400

    Add a new spec test to user feature

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,11 @@ require "rails_helper"
 
 describe UsersController do
 
-
+describe "GET #index" do
+  it "renders the :index view" do
+    get :index
+    response.should render_template :index
+  end
+end
 
 end
