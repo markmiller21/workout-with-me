50894975d8ae17d25f63cd32c27d6fb8d0fadd52
Navraj Nat <nnat425@gmail.com>
2015-10-10 16:50:00 -0400

    Add destroy in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,8 +18,11 @@ class SessionsController < ApplicationController
     end
 	end
 
+  def destroy
+  end
+
   private
-  
+
   def session_params
     params.require(:session).permit(:email, :password)
   end
