5b9f1af31b949181ad5c9290d41f762d1e050fb0
Navraj Nat <nnat425@gmail.com>
2015-10-10 17:24:15 -0400

    Remove index test in user and move it to matches

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,6 @@ require "rails_helper"
 
 describe UsersController do
 
-describe "GET #index" do
-  it "renders the :index view" do
-    get :index
-    response.should render_template :index
-  end
 end
 
 end
