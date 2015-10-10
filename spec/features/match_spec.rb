d66dd039570a64e0b51dabfeb790fe44b55e032e
Jenny Lee <leejenny0403@gmail.com>
2015-10-10 16:24:41 -0400

    move log_me_in into before each

--- spec/features/match_spec.rb
+++ spec/features/match_spec.rb
@@ -9,28 +9,31 @@ describe 'possible match' do
   click_link "Login Here"
 }
 
+  before(:each) do
+    log_me_in
+  end
 
 it 'page contains potential user on the page' do
-  log_me_in
+
   @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
   visit match_path(@potential_user)
   expect(page).to have_content @potential_user.name
 end
 
 it 'page contains a see more button ' do
-  log_me_in
+
   expect(page).to have_content "See more"
 end
 
 it 'page redirects to correct page when clicking See more link' do
-  log_me_in
+
   click_link "See more"
   expect(page).to have_content "Profile page"
   #Can't test going to a specific user yet, due to algorithm not being created
 end
 
 it 'page redirects to the next user if liked clicked' do
-  log_me_in
+
   @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
   visit match_path(@potential_user)
   click_link "LIKE"
