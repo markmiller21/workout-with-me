2cc94cbe2f8768a417c63adc0704ba5f7469c013
Jenny Lee <leejenny0403@gmail.com>
2015-10-11 13:41:34 -0400

    create test to view match rating when on matches page

--- spec/features/rating_spec.rb
+++ spec/features/rating_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 
-RSpec.feature "User Ratings", :type => :feature do
+RSpec.feature "Ratings", :type => :feature do
+  let(:potential_match) {
+    create(:potential_user)
+  }
+
+  let(:potential_match2) {
+    create(:potential_user2)
+  }
+
   let(:log_me_in) {
     @user = create(:user)
     @user.activities.create(name: "Lifting")
+    potential_match
+    potential_match2
     visit root_path
     click_link "Login Here"
     fill_in 'Email', :with => @user.email
@@ -11,19 +21,10 @@ RSpec.feature "User Ratings", :type => :feature do
     click_button 'Login'
   }
 
-  let(:potential_match) {
-    create(:potential_user)
-    potential_match.activities.create(name: "Lifting")
-  }
-
-  let(:potential_match2) {
-    create(:potential_user2)
-    potential_match2.activities.create(name: "Lifting")
-  }
-
   describe "view user rating" do
     before :each do
       log_me_in
+      potential_match.activities.create(name: "Lifting")
       match = Match.create(initiator_id: @user.id, responder_id: potential_match.id, accepted: 1)
       first_rating = Rating.create(rater_id: @user.id, ratee_id: potential_match.id, rank: 5)
     end
