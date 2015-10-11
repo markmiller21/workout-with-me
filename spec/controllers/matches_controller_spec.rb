252aa0a87386eb85183908033067b606534f899a
Navraj Nat <nnat425@gmail.com>
2015-10-10 21:10:27 -0400

    Test to see match on page works

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -4,6 +4,13 @@ describe MatchesController do
 
   describe "GET #show" do
 
+    let(:log_me_in) {
+      @user = create(:user)
+      session[:user_id] = @user.id
+    }
+
+
+
 
     it "renders the #show view" do
      @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
@@ -14,23 +21,15 @@ describe MatchesController do
 
    describe "GET #index" do
     it "renders the :index view" do
+      # @potential_user = create(:potential_user)
+      # Match.create(initiator_id: ,responder_id:)
       get :index
       response.should render_template :index
-      @potential_match = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
-      get :show, id: @potential_match.id
-      expect(response).to render_template :show
+
     end
   end
 
-#  it "populates an array of matches" do
-#     contact = Factory(:contact)
-#     get :index
-#     assigns(:contacts).should eq([contact])
-#   end
-
-#   describe "POST #create" do
-
-  end
+end
 
 
 end
--- spec/features/match_spec.rb
+++ spec/features/match_spec.rb
@@ -65,7 +65,7 @@ describe 'possible match page' do
     end
 
     it 'list a match on the page' do
-      Match.create(initiator_id:@user.id, responder_id: @dummy.id, accepted: 2)
+      Match.create(initiator_id:@user.id, responder_id: @dummy.id, accepted: 1)
       click_link "My matches"
       expect(page).to have_content @dummy.name
     end
