e1ca6306f6157d15c155cfe1307bbb9a774cc147
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-10 20:03:46 -0400

    add before each for post #create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,15 @@
 require "rails_helper"
 
 describe SessionsController do
+	describe "POST #create" do
+		describe "When successful" do
+			before(:each){
+				@user = create(:user)
+			}
+
+		
+		end
+	end
+
+
 end
