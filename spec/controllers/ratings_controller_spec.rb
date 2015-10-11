af026af041c57e6d938fb948c3920bb2ac17c4b8
Jenny Lee <leejenny0403@gmail.com>
2015-10-11 15:07:54 -0400

    add passing test for post create method increases rating count

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -8,14 +8,17 @@ RSpec.describe RatingsController do
   let(:matched_user) { create(:potential_user) }
 
   describe "POST #create" do
+    before :each do
+      log_me_in
+    end
+
     context "valid attributes" do
       it "should add rating to matched users's ratings" do
         @user.activities.create(name: "Tennis")
         matched_user.activities.create(name: "Tennis")
         Match.create(initiator_id: @user.id, responder_id: matched_user.id, accepted: 1)
         expect {
-          post :create,
-
+          post :create, ratee_id: matched_user, rank: 5
         }.to change(Rating,:count).by(1)
       end
     end
