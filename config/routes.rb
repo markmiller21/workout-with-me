86eaccae9ea0abce5cbe2f1ceb2509fd46272737
Navraj Nat <nnat425@gmail.com>
2015-10-11 17:51:16 -0400

    Ajax for chat is functoning

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :matches
   end
+  resources :messages
 
   get '/session' => 'sessions#index'
   get '/login' => 'sessions#new'
