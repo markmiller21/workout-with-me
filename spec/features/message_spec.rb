751877032a8e55d2fe7ebca8e75e42a78e212d1d
Jenny Lee <leejenny0403@gmail.com>
2015-10-11 23:29:23 -0400

    create log_me_in and matched user methods for messages feature test

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -1,4 +1,15 @@
 require "rails_helper"
 
 RSpec.feature "Messages", :type => :feature do
+  let(:matched_user) { create(:potential_user) }
+  let(:log_me_in) {
+    @user = create(:user)
+    @user.activities.create(name: "Lifting")
+    visit root_path
+    click_link "Login Here"
+    fill_in 'Email', :with => @user.email
+    fill_in 'Password', :with => @user.Password
+    click_button 'Login'
+  }
+
 end
