886a822e9e15d8492698c6df219b009b16cfaf8f
Jenny Lee <leejenny0403@gmail.com>
2015-10-12 10:40:32 -0400

    fix broken test for get show, added log_me_in in order to see the show page

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -9,6 +9,7 @@ describe MatchesController do
 
   describe "GET #show" do
     it "renders the #show view" do
+      log_me_in
       @potential_user = User.create(name:"jenny",password_digest:"jenny",age:25,gender:"female",description:"just think about it",email:"jenny@jenny.com")
       get :show, id: @potential_user.id
       expect(response).to render_template :show
