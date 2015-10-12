c2dccade57b50bf53135b2d153a86f677ad4b19f
Jenny Lee <leejenny0403@gmail.com>
2015-10-12 01:54:48 -0400

    add sender name to chat box

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
-belongs_to :sender, :class_name => 'User'
-belongs_to :receiver, :class_name => 'User'
-belongs_to :match
+  belongs_to :sender, :class_name => 'User'
+  belongs_to :receiver, :class_name => 'User'
+  belongs_to :match
+
+  # def sent_message?
+  #   self.sender_id: self.id)
+  #   @received_messages = Message.where(sender_id: @receiver.id, receiver_id: current_user.id)
 end
