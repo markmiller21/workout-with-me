b02dd9a8c95859e315ae6da3253400e795d6f933
Navraj Nat <nnat425@gmail.com>
2015-10-11 20:16:53 -0400

    Messages associate to a match

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,17 +2,17 @@ class MessagesController < ApplicationController
   before_action :message_attributes, only: [:create]
   def index
     @match = Match.find(params[:match_id])
-    @messages = Message.all
+    @messages = @match.messages.all
   end
 
   def create
-    # Message.create(sender_id: current_user.id, reciever_id: )
+    # @match = Match.find_by(id: params[:id])
     @message = Message.create!(message_attributes)
   end
 
   private
 
   def message_attributes
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content,:match_id)
   end
 end
