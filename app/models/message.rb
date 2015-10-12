77a65f09805574a0c0bf090f217abfe6971d700b
Jenny Lee <leejenny0403@gmail.com>
2015-10-12 19:06:21 -0400

    remove like and dislike button from user profile page

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ActiveRecord::Base
   belongs_to :receiver, :class_name => 'User'
   belongs_to :match
 
+  validates :content, :presence => true
   # def sent_message?
   #   self.sender_id: self.id)
   #   @received_messages = Message.where(sender_id: @receiver.id, receiver_id: current_user.id)
