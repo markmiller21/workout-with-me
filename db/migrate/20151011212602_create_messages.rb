d534929419b209903eb4831b5f8ff4ad7af28d32
Navraj Nat <nnat425@gmail.com>
2015-10-11 21:16:45 -0400

    Add null  false to all columns in message db

--- db/migrate/20151011212602_create_messages.rb
+++ db/migrate/20151011212602_create_messages.rb
@@ -1,10 +1,11 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :content
-      t.references :sender
-      t.references :receiver
-      t.references :match
+      t.string :content, null: false
+      t.references :sender, null: false
+      t.references :receiver, null: false
+      t.references :match, null: false
+      t.timestamps null: false
     end
   end
 end
