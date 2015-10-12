b43a52778f07062edb4d836b991cf76fc98c580c
Jenny Lee <leejenny0403@gmail.com>
2015-10-12 19:55:29 -0400

    validates for correctly formatted email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,9 @@ class User < ActiveRecord::Base
 
   has_many :locations
 
-	validates :name, :email, :gender, :age, :presence => true
+	validates :name, :gender, :age, :presence => true
   validates :age, numericality: { only_integer: true }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, :email_format => {:message => 'Invalid email address' }
   validates :description, length: { maximum: 200 }, allow_blank: true
 
 
