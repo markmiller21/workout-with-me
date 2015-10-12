c2ebdfa587e0b84cb9d70f8f53c7a33a6b42a055
Navraj Nat <nnat425@gmail.com>
2015-10-11 20:57:25 -0400

    Message is being created with sender_id )

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,13 +6,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-    # @match = Match.find_by(id: params[:id])
     @message = Message.create!(message_attributes)
   end
 
   private
 
   def message_attributes
-    params.require(:message).permit(:content,:match_id)
+    params.require(:message).permit(:content,:match_id,:sender_id,:receiver_id)
   end
+
 end
