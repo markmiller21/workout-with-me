fc87a8a30c83d6840eb6340f608d5ad7e88d5c24
Jenny Lee <leejenny0403@gmail.com>
2015-10-11 23:46:18 -0400

    add passing test for chat page loads correctly

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -1,15 +1,30 @@
 require "rails_helper"
 
 RSpec.feature "Messages", :type => :feature do
-  let(:matched_user) { create(:potential_user) }
+  let(:matched_user) { @matched_user = create(:potential_user) }
   let(:log_me_in) {
     @user = create(:user)
     @user.activities.create(name: "Lifting")
     visit root_path
     click_link "Login Here"
     fill_in 'Email', :with => @user.email
-    fill_in 'Password', :with => @user.Password
+    fill_in 'Password', :with => @user.password
     click_button 'Login'
   }
 
+  describe "messages index page" do
+    before :each do
+      matched_user.activities.create(name: "Lifting")
+      log_me_in
+      Match.create(initiator_id: @matched_user.id, responder_id: @user.id, accepted: 1)
+      visit matches_path
+      click_link @matched_user.name
+    end
+
+    context "both users matched" do
+      scenario "should show chat box" do
+        expect(page).to have_content("Chat")
+      end
+    end
+  end
 end
