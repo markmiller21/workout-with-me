8f0c4d8548277eefb248333de4712235f0b0c361
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-13 15:55:58 -0400

    uniform boxes / take out panels

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -19,7 +19,7 @@ class MatchesController < ApplicationController
     @match = Match.new
     @potential_match = User.find_by(id: params[:id])
     @average_rating = @potential_match.average_rating
-    @distance_in_miles = calculate_distance([@potential_match.locations.first.latitude,@potential_match.locations.first.longitude],[current_user.locations.first.latitude,current_user.locations.first.longitude])
+    @distance_in_miles = calculate_distance([@potential_match.locations.first.latitude,@potential_match.locations.first.longitude],[current_user.locations.first.latitude,current_user.locations.first.longitude]).to_i
   end
 
   def create
