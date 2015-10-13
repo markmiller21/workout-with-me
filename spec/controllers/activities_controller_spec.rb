c193082a151331d56d747abcd4c23c0f375551ef
Jenny Lee <leejenny0403@gmail.com>
2015-10-13 14:47:09 -0400

    all tests passing for gender preference

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -27,37 +27,37 @@ RSpec.describe ActivitiesController do
   describe "POST #create" do
     context "with invalid attributes" do
       it "should not redirect if activities not chosen" do
-        post :create
+        post :create, user: {gender_preference: "Male"}
         expect(response).to_not redirect_to(match_path(potential_match))
       end
 
       it "sets flash error if activities not chosen" do
-        post :create
+        post :create, user: {gender_preference: "Male"}
         expect(flash[:error]).to have_content("Must choose at least 1 activity")
       end
 
       it "should redirect to activities page again" do
-        post :create
+        post :create, user: {gender_preference: "Male"}
         expect(response).to redirect_to(activities_path)
       end
     end
 
     context "with valid attributes" do
       it "returns 302 status" do
-        post :create, "Tennis"
+        post :create, name: ["Tennis"], user: {gender_preference: "Male"}
         expect(response.status).to eq(302)
       end
 
       it "should show potential matches page" do
         potential_match.activities.create(name: "Tennis")
-        post :create, name: ["Tennis"]
+        post :create, name: ["Tennis"], user: {gender_preference: "Male"}
         expect(response).to redirect_to initiate_match_path
       end
 
       it "should add activity to user activities" do
         potential_match.activities.create(name: "Tennis")
         expect {
-          post :create, name: ["Tennis"]
+          post :create, name: ["Tennis"], user: {gender_preference: "Male"}
         }.to change(@user.activities,:count).by(1)
       end
     end
