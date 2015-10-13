823d5a523d1cdc4f11fdda93cfa197ac04f9503f
Navraj Nat <nnat425@gmail.com>
2015-10-13 07:39:39 -0400

    Only need to validate presence of gender and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   has_many :locations
 
-	validates :name, :presence => true
+	validates :name, :gender, :presence => true
   validates :age, numericality: { only_integer: true }
   validates :email, uniqueness: true, :email_format => {:message => 'Invalid email address' }
   validates :description, length: { maximum: 200 }, allow_blank: true
