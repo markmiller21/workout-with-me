69fb1dcc4164c7fb09455247fef53f4de820b928
Jenny Lee <leejenny0403@gmail.com>
2015-10-14 01:00:12 -0400

    add test for show method in users controller spec, pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,25 @@
 require "rails_helper"
 
 RSpec.describe UsersController do
+  let(:log_me_in) {
+    @user = create(:user)
+    @user.locations.create(longitude: 123.321,latitude: 98773.3215)
+    session[:user_id] = @user.id
+  }
+  let(:potential_match) { create(:potential_user) }
 
   describe "GET #new" do
     it "renders the :new template" do
       get :new
       response.should render_template :new
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the #show view" do
+      log_me_in
+      get :show, id: potential_match
+      response.should render_template :show
     end
   end
 
