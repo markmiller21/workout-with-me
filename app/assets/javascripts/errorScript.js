f8c1400813975a3c9330577765d431b09d4cb1c7
Navraj Nat <nnat425@gmail.com>
2015-10-14 16:31:13 -0400

    Alert message occurs when match happens

---
+++ app/assets/javascripts/errorScript.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  if ($("#message").text() == "You've been matched!"){
+    $("#message").hide();
+    $(function() {
+      $( "#error-confirm" ).dialog({
+        resizable: false,
+        height:300,
+        modal: true,
+        open: function(event, ui) { $(".ui-dialog-titlebar-close", ui.dialog | ui).hide(); },
+        buttons: {
+          "Start chatting!": function() {
+            window.location = "/matches"
+          },
+          "Keep swiping!": function() {
+            window.location = ( "close" );
+          }
+        }
+      });
+    });
+  }
+});
