5745585ed195f95aae0c8a1f62ea8592fe537bcd
Jenny Lee <leejenny0403@gmail.com>
2015-10-14 09:40:34 -0400

    still not passing test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe UsersController do
     @user.locations.create(longitude: 123.321,latitude: 98773.3215)
     session[:user_id] = @user.id
   }
-  let(:potential_match) { create(:potential_user) }
+  let(:potential_match) { @potential_match = create(:potential_user) }
 
   describe "GET #new" do
     it "renders the :new template" do
@@ -20,6 +20,13 @@ RSpec.describe UsersController do
       log_me_in
       get :show, id: potential_match
       response.should render_template :show
+    end
+
+    it "assigns the requested user to @user" do
+      log_me_in
+      potential_match
+      get :show, id: potential_match
+      assigns(:potential_match).should eq(@potential_match)
     end
   end
 
