08385b13e9f41be4f152bd25fdafc254a413b2c2
Jenny Lee <leejenny0403@gmail.com>
2015-10-14 12:33:35 -0400

    get index renders chat page

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,9 +1,34 @@
 require "rails_helper"
 
 RSpec.describe MessagesController do
-  describe "GET #index" do
+  let(:log_me_in) {
+    @user = create(:user)
+    @user.locations.create(longitude: 123.321,latitude: 98773.3215)
+    session[:user_id] = @user.id
+  }
+  let(:user_attr) { attributes_for(:user) }
+  let(:potential_match) { @potential_match = create(:potential_user) }
 
+  let(:matched_user) {
+    @matched_user = create(:potential_user2)
+  }
+  before :each do
+    @shared_activity = create(:activity)
+    potential_match
+    matched_user
+    @matched_user.activities << @shared_activity
+    log_me_in
+    @user.activities << @shared_activity
+    @match = @matched_user.initiator_matches.create(responder_id: @user.id, accepted: 1)
   end
+
+  describe "GET #index" do
+    it "renders the chat page" do
+      get :index, match_id: @match
+      expect(response).to render_template :index
+    end
+  end
+
   describe "POST #create" do
   end
 end
