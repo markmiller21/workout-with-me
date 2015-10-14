fd4670206f1cfb660821474f119e8b83e076eba9
Navraj Nat <nnat425@gmail.com>
2015-10-14 16:36:05 -0400

    JS files are moved in correct files

--- app/assets/javascripts/matchAlert.js
+++ app/assets/javascripts/matchAlert.js
@@ -1,3 +1,21 @@
 $( document ).ready(function() {
-
+  if ($("#message").text() == "You've been matched!"){
+    $(function() {
+      $( "#dialog-confirm" ).dialog({
+        resizable: false,
+        height:300,
+        modal: true,
+        title: "yo",
+        open: function(event, ui) { $(".ui-dialog-titlebar-close", ui.dialog | ui).hide(); },
+        buttons: {
+          "See your current matches!!!": function() {
+            window.location = "/matches"
+          },
+          "Keep on swiping!": function() {
+            $( this ).dialog( "close" );
+          }
+        }
+      });
+    });
+  }
 });
