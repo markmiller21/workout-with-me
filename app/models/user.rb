bd83258c08414354e0376aa68ec031c2cd7b3356
Jenny Lee <leejenny0403@gmail.com>
2015-10-15 11:49:37 -0400

    check if image uploads via paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ActiveRecord::Base
   has_attached_file :avatar,
     styles: { medium: "300x300>", thumb: "100x100>" },
-    default_url: "http://prepgenie.ie/gamsat/wp-content/uploads/2015/08/default-avatar_man.png",
-    storage: :s3,
-    s3_credentials: "#{RAILS_ROOT}/config/s3.yml",
-    path: "/avatars/:style/:id/:filename"
-
+    default_url: "http://prepgenie.ie/gamsat/wp-content/uploads/2015/08/default-avatar_man.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
   has_many :user_activities
